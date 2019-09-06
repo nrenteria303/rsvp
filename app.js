3014bd20d54158081e21ae83d93e32a473b19272
Nick Renteria <Nick Renteria>
2019-09-05 22:15:03 -0600

    added checkbox localStorage; new name save bug resulting

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const mainDiv = document.querySelector('.main');
@@ -31,6 +29,10 @@ function filterInvitees(response) {
     }
 }
 
+function checkInviteeName() {
+
+}
+
 function supportsLocalStorage() {
     try {
         return 'localStorage' in window && window['localStorage'] !== null;
@@ -44,22 +46,21 @@ function getRecentInvitees() {
     if (invitees && inviteeString !== '' && invitees !== inviteeString) {
         return JSON.parse(inviteeString);
     } else if (invitees) {
-        return JSON.parse(invitees);
+        let newInviteeArray = JSON.parse(invitees);
+        return newInviteeArray.filter(function(obj) {return obj !== '';});
     } else {
-        return[];
+        return [];
     }
 }
 
 function saveInviteeString(str) {
-    // var invitees = getRecentInvitees();
-    if (inviteeArray.indexOf(str) > -1) {
+    if (inviteeArray.filter(function(obj) {return obj.name === str;}).length !== 0 && inviteeArray.length !== 0) {
         alert('This name is already in your invitee list.');
         return false;
     } else if (!str) {
         return false;
     } else {
-        inviteeArray.push(str);
-        // invitees.push(inviteeArray);
+        inviteeArray.push({name: str, isChecked: false, yes: false, no: false});
         localStorage.setItem('recentInvitees', JSON.stringify(inviteeArray));
         return true;
     }
@@ -117,24 +118,6 @@ function createLi(text) {
     return li;
 }
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     if (input.value == '' || input.value == ' ') {
-//         alert('Please enter an invitee name');
-//     } else {
-//         const text = input.value;
-//         input.value = '';
-//         const li = createLi(text);
-//         ul.appendChild(li);
-
-//         var inviteeString = input.value;
-//         if (saveInviteeString(inviteeString)) {
-//             const li = createLi(inviteeString);
-//             ul.appendChild(li);
-//         }
-//     }
-// });
-
 window.onload = function() {
     if (supportsLocalStorage) {
 
@@ -142,9 +125,19 @@ window.onload = function() {
         var recentInvitees = getRecentInvitees();
         recentInvitees.forEach(function(inviteeStr) {
             inviteeArray.push(inviteeStr);
-            const li = createLi(inviteeStr);
+            const li = createLi(inviteeStr.name);
+            const checkYes = li.children[1].childNodes[1];
+            const checkNo = li.children[2].childNodes[1];
             ul.appendChild(li);
+            if (inviteeStr.isChecked === true) {
+                if (inviteeStr.yes === true) {
+                    checkYes.click();
+                } else if (inviteeStr.no === true) {
+                    checkNo.click();
+                }
+            }
         });
+        localStorage.setItem('recentInvitees', JSON.stringify(inviteeArray));
 
         // Set event handlers
         form.addEventListener('submit', (e) => {
@@ -154,47 +147,84 @@ window.onload = function() {
             } else {
                 const text = input.value;
                 input.value = '';
-        
+
                 if (saveInviteeString(text)) {
                     const li = createLi(text);
                     ul.appendChild(li);
                 }
             }
-        });
-
+});
     }
 }
+
+// // Set event handlers
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     if (input.value == '' || input.value == ' ') {
+//         alert('Please enter an invitee name');
+//     } else {
+//         const text = input.value;
+//         input.value = '';
+
+//         if (saveInviteeString(text)) {
+//             const li = createLi(text);
+//             ul.appendChild(li);
+//         }
+//     }
+// });
 
 ul.addEventListener('change', (e) => {
     const checkbox = e.target;
     const checkboxLabel = checkbox.parentNode;
+    const li = checkboxLabel.parentNode;
+    const name = li.firstChild.textContent;
     const checked = checkbox.checked;
     listItem = checkbox.parentNode.parentNode;
+    inviteeIndex = inviteeArray.indexOf(inviteeArray.find(function(obj) {return obj.name === name;}));
 
     if (checked && checkboxLabel.textContent === "I'll be there") {
         listItem.className = 'responded-yes';
         checkboxLabel.nextElementSibling.style.display = 'none';
+        inviteeArray[inviteeIndex].isChecked = true;
+        inviteeArray[inviteeIndex].yes = true;
     } else if (checked && checkboxLabel.textContent === "Can't make it") {
         listItem.className = 'responded-no';
         checkboxLabel.previousElementSibling.style.display = 'none';
+        inviteeArray[inviteeIndex].isChecked = true;
+        inviteeArray[inviteeIndex].no = true;
     } else {
         listItem.className = '';
         checkboxLabel.nextElementSibling.style.display = '';
         checkboxLabel.previousElementSibling.style.display = '';
+        inviteeArray[inviteeIndex].isChecked = false;
+        inviteeArray[inviteeIndex].yes = false;
+        inviteeArray[inviteeIndex].no = false;
     }
+    localStorage.setItem('recentInvitees', JSON.stringify(inviteeArray));
 });
 
 ul.addEventListener('click', (e) => {
     const button = e.target;
     if (button.tagName === 'BUTTON') {
         const li = button.parentNode;
+        const checkYes = li.children[1].childNodes[1];
+        const checkNo = li.children[2].childNodes[1];
+        let isLiChecked;
+        function checkForChecked() {
+            if (checkYes.checked || checkNo.checked) {
+                isLiChecked = true;
+            } else {
+                isLiChecked = false;
+            }
+        }
         const name = li.firstChild.textContent;
+        const currentObj = inviteeArray.find(function(obj) {return obj === name;});
         const ul = li.parentNode;
         const action = button.textContent;
         const nameActions = {
             remove: () => {
                 ul.removeChild(li);
-                inviteeArray.splice(inviteeArray.indexOf(name), 1);
+                inviteeArray.splice(inviteeArray.indexOf(currentObj), 1);
                 if (inviteeArray.length > 2) {
                     inviteeString = '["' + inviteeArray[0] + '",'
                     for (let i = 1; i < (inviteeArray.length - 1); i++) {
@@ -210,7 +240,7 @@ ul.addEventListener('click', (e) => {
 
             },
             edit: () => {
-                inviteeIndex = inviteeArray.indexOf(name);
+                inviteeIndex = inviteeArray.indexOf(currentObj);
                 const span = li.firstElementChild;
                 const input = document.createElement('input');
                 input.type = 'text';
@@ -220,9 +250,11 @@ ul.addEventListener('click', (e) => {
                 button.textContent = 'save';
             },
             save: () => {
+                checkForChecked();
+                // inviteeIndex = inviteeArray.indexOf(currentObj);
                 const input = li.firstElementChild;
                 const newSpan = document.createElement('span');
-                inviteeArray.splice(inviteeIndex, 1, input.value); // inviteeArray.indexOf(name) is invalid
+                inviteeArray.splice(inviteeIndex, 1, {name: input.value, isChecked: isLiChecked, yes: checkYes.checked, no: checkNo.checked}); 
                 newSpan.textContent = input.value;
                 li.insertBefore(newSpan, input);
                 li.removeChild(input);
@@ -241,7 +273,3 @@ clearBtn.addEventListener('click', (e) => {
         location.reload();
     }
 });
-
-
-
-// }); // END OF DOMContentLoaded EVENT LISTENER
